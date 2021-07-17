3192bd15ccee2ce743f07db16112162a4ec3dd6b
federicocotogno <federicosou@fastmail.se>
2021-07-17 14:52:56 +0200

    Added and cleaned the necessary code

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Platform } from 'react-native';
+import * as ScreenCapture from 'expo-screen-capture';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
+  async function askForPermission() {
+    // This permission is only required on Android
+    const { status } = await MediaLibrary.requestPermissionsAsync();
+    if (status === 'granted') {
+      ScreenCapture.addScreenshotListener(() => {
+        alert('You just took a screenshot! 😊');
+      });
+    }
+  };
+
+  const _activate = async () => {
+    await ScreenCapture.preventScreenCaptureAsync();
+    console.log('Preventing screenshots!');
+  };
+
+  const _deactivate = async () => {
+    await ScreenCapture.allowScreenCaptureAsync();
+    console.log('You can now take screenshots!');
+  };
+
+  useEffect(() => {
+    askForPermission();
+  }, []);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Allow" onPress={() => _deactivate()} />
+      <Text>  </Text>
+      <Button color='tomato' title="Prevent" onPress={() => _activate()} />
       <StatusBar style="auto" />
     </View>
   );
